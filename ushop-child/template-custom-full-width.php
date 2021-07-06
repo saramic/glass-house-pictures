7acf629755737c763b57e731fed55ca3be5e5a3d
Michael Milewski <saramic@gmail.com>
2021-07-06 20:56:39 +1000

    custom full width template with scripted logo
    to be placed in the middle of a hero image

---
+++ ushop-child/template-custom-full-width.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Template Name: Custom Full Width
+ *
+ * @package ushop
+ * @subpackage ushop
+ */
+get_header();
+?>
+
+<style>
+.ghp-logo-wrapper {
+  top: 11vh;
+  position: absolute;
+  width: 100vw;
+  filter: drop-shadow(0.4rem 0.4rem 0.4rem rgba(33, 33, 33, 0.2));
+}
+.ghp-logo {
+  width: 40vw;
+  display: block;
+  margin:auto
+}
+</style>
+<script>
+  var myDiv = document.createElement("div");
+  myDiv.classList.add("ghp-logo-wrapper");
+  myDiv.innerHTML = '<img class="ghp-logo glasshousepictures-logo" src="https://glasshousepictures.com.au/wp-content/uploads/2021/06/GHP_logo_4col_solid.png" alt="Glasshouse Pictures">';
+  document.querySelector(".hero-area.position-relative").appendChild(myDiv);
+// = "hi" 
+//  <div class="ghp-logo-wrapper">
+//  </div>
+</script>
+    <main class="full-width-page">
+        <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 margin-top">
+                        <?php
+                        while ( have_posts() ) : the_post();
+
+                            get_template_part( 'template-parts/content', 'page' );
+
+                            // If comments are open or we have at least one comment, load up the comment template.
+                            if ( comments_open() || get_comments_number() ) :
+                                comments_template();
+                            endif;
+
+                        endwhile; // End of the loop.
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main><!-- #main -->
+
+<?php
+
+get_footer();
